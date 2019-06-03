6712d04eaace7cd35c71f0c2655cdee7b1aec1a4
Siddarth <sidr97@gmail.com>
2019-06-03 11:42:29 -0700

    Pip Changes
    Made changes to allow pip to be used to download and install this package.

---
+++ Setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='Zlab_Automation_Pipeline',
+      version='1.8',
+      description='Pipeline to automate the EpMotion Liquid Handler and Cytoflex using experimental designs from JMP ',
+      url='https://gitlab.com/stemcellbioengineering/epmotion-pipeline',
+      author='Siddarth Raghuvanshi',
+      author_email='raghuvanshi.siddarth@gmail.com',
+      license='MIT',
+      packages=['Zlab_Automation_Pipeline'],
+      zip_safe=True)
